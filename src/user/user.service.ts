1c73d452d377298069c4543f862b77d2ee80f47a
Erick Medeiros <erickmedeiros@b2bit.company>
2023-01-16 22:53:46 -0300

    feat: :sparkles: create user with encrypted password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,12 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(createUserDto: CreateUserDto) {
+    const data = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    };
+
+    const createUser = await this.prisma.user.create({ data });
+    return { ...createUser, password: undefined };
   }
 
   findAll() {

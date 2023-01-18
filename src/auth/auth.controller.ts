2298bd1ff9071ab8b1ce87f865f171534a6ea1c0
Erick Medeiros <erickmedeiros@b2bit.company>
2023-01-18 20:49:33 -0300

    feat: :truck: public routes and me url

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { IsPublic } from './decorators/is-public.decorator';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller()
@@ -14,6 +15,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @IsPublic()
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
   login(@Request() req: AuthRequest) {
